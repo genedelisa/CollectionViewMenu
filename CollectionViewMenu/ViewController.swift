941c3b4c8bcf884a1b2c5b170d010e3db9bc38f2
Gene De Lisa <gene@rockhoppertech.com>
2015-04-08 10:10:00 -0400

    Initial Commit

---
+++ CollectionViewMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewMenu
+//
+//  Created by Gene De Lisa on 4/8/15.
+//  Copyright (c) 2015 Gene De Lisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
